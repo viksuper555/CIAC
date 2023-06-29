9cd6b5c9f650b4b0e61ae3e1941f99ac8b5de242
YukiYuuna <blgeorgiev@tu-sofia.bg>
2023-06-29 14:45:44 +0300

    Initial commit

---
+++ components/navbar/index.tsx
@@ -0,0 +1,46 @@
+import React, {FC, useState} from 'react';
+import logo from '../../public/logo.png';
+import Image from 'next/image';
+import styles from './navbar.module.scss';
+
+interface Props {
+    changeTheme: any;
+    currentTheme: string;
+}
+
+const Navbar:FC<Props> = ({changeTheme, currentTheme}) => {
+
+    return (
+        <nav className={styles.nav}>
+            <div>
+                <div className={styles.brand}>
+                    <Image src={logo} alt={"logo"}/>
+                </div>
+            </div>
+            <div className={styles.toggleContainer}>
+                <div className={"toggle"}>
+                    <div className={"mode"}>
+                    </div>
+                </div>
+                <div>
+                    <ul className={styles.links}>
+                        <li>
+                            <a href={"#"}> За Нас </a>
+                        </li>
+                        <li>
+                            <a href={"#"}> Модули </a>
+                        </li>
+                        <li>
+                            <a href={"/Launch"}> Екип </a>
+                        </li>
+                        <li>
+                            <a href={"/"}> Контакти </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
