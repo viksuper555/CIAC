c13c15b6a5e4a929b1076df968bdeeffb40a8d62
YukiYuuna <blgeorgiev@tu-sofia.bg>
2023-11-06 19:47:27 +0200

    Remove prod log

--- components/ui/chatGpt/index.tsx
+++ components/ui/chatGpt/index.tsx
@@ -14,7 +14,6 @@ const ChatGpt = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false)
     const [response, setResponse] = useState<any>("");
     const [generatingResponse, setGeneratingResponse] = useState<boolean>(false);
-    console.log(process.env.NEXT_PUBLIC_API_KEY)
 
     const {texts: {Bulgaria: texts}} = getTexts();
 
